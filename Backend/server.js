02bce507e17a1614d334064a8e7254e13843be8e
BodaSanthosh <120643087+santhosh2003-bit@users.noreply.github.com>
2024-08-19 17:48:58 +0530

    port numbers Handle

--- Backend/server.js
+++ Backend/server.js
@@ -2,6 +2,7 @@ const express = require("express");
 const app = express();
 const cors = require("cors");
 const ConnectDb = require("./db/db");
+const dotenv = require("dotenv");
 const { register, login } = require("../Backend/Auth/userAuthentication");
 const middleware = require("../Backend/middleware/middleware");
 const {
@@ -11,12 +12,14 @@ const {
   eventDelete,
 } = require("../Backend/Auth/eventStorage");
 const { eventFind } = require("../Backend/Auth/getEvenById");
+dotenv.config();
 app.use(express.json());
 app.use(
   cors({
     origin: "*",
   })
 );
+
 app.post("/register", register);
 app.post("/login", login);
 app.post("/events-add", middleware, eventAdding);
@@ -25,4 +28,4 @@ app.delete("/event-delete", middleware, eventDelete);
 app.put("/event-update", middleware, eventUpdate);
 app.get("/event-find/:id", middleware, eventFind);
 ConnectDb();
-app.listen(5000, () => console.log("Server Running on port 5000"));
+app.listen(PORT || 8080, () => console.log("Server Running on port 5000"));
