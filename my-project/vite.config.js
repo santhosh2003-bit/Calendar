3ecb36aca86d05d30aa82bbe8d14ae56ad7f3500
BodaSanthosh <120643087+santhosh2003-bit@users.noreply.github.com>
2024-08-19 20:18:43 +0530

    Routing Handle

--- my-project/vite.config.js
+++ my-project/vite.config.js
@@ -1,7 +1,11 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  base: "/", // Ensure this is set to the correct base URL
+  build: {
+    outDir: "dist",
+  },
+});
